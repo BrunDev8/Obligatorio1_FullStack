3eb70167610a6424ac6d172b95da6e26feec296e
Obligatorio User <user@example.com>
2026-04-16 10:33:28 -0300

    refactor: simplify server startup by removing database connection logic

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 import app from "./app.js";
-import connectDB from "./v1/config/db.config.js";
 
 const PORT = process.env.PORT || 3000;
 
-const start = async () => {
-    await connectDB();
-    app.listen(PORT, () => {
-        console.log(`http://localhost:${PORT}`);
-    });
-};
-
-start();
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+    
+})
